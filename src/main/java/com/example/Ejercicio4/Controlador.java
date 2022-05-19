2ff4ff1d0b63f488cc94efdca508f221d2310e2e
Alejandro Martin <alejandro.mperez@bosonit.com>
2022-05-19 10:54:24 +0200

    Actualizado

--- src/main/java/com/example/Ejercicio4/Controlador.java
+++ src/main/java/com/example/Ejercicio4/Controlador.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class Controlador {
 
-    @GetMapping("/user/{nombre}") //Establecemos la ruta y lo que esta entre {} son variables
+    @GetMapping("/user/{nombre}") //Establecemos la ruta y lo que esta entre {} son variables, utilizamos getmapping para recibir desde postman
     @ResponseBody
     public String user(@PathVariable String nombre){
         return "Hola "+nombre;
@@ -20,9 +20,10 @@ public class Controlador {
     }
     */
 
-    @PostMapping("/useradd") //podemos pasarle mas de una variable
+    @PostMapping("/useradd") //ponemos PostMapping para realizarlo mediante el metodo post y enviar desde postman
     @RequestMapping(value = "/useradd", method = RequestMethod.POST, consumes = "application/json")
-    public String useradd(@RequestBody Persona persona1){
+    public String useradd(@RequestBody Persona persona1){ //creamos el objeto con los parametros que le hemos pasado en post
+        persona1.setEdad(persona1.getEdad()+1);
         return persona1.toStirng();
     }
 
--- src/main/java/com/example/Ejercicio4/Persona.java
+++ src/main/java/com/example/Ejercicio4/Persona.java
@@ -11,6 +11,31 @@ public class Persona {
         this.edad=edad;
         this.poblacion=poblacion;
     }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public int getEdad(){
+        return edad;
+    }
+
+    public String getPoblacion(){
+        return poblacion;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setPoblacion(String poblacion) {
+        this.poblacion = poblacion;
+    }
+
     public String toStirng(){
         return "Nombre: "+nombre+", edad: "+edad+", poblacion: "+poblacion;
     }
